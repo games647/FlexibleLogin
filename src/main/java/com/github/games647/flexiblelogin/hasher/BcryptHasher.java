bae8e393f38c888cdacadadda6d8c543c33612b6
games647 <games647@users.noreply.github.com>
2017-07-24 09:49:16 +0200

    Generate $2a bcrypt hashes to comply with other implementations

--- src/main/java/com/github/games647/flexiblelogin/hasher/BcryptHasher.java
+++ src/main/java/com/github/games647/flexiblelogin/hasher/BcryptHasher.java
@@ -31,20 +31,18 @@ public class BcryptHasher implements Hasher {
     @Override
     public String hash(String rawPassword) {
         //generate a different salt for each user
-        return BCrypt.hashpw(rawPassword, BCrypt.gensalt());
+        String passwordHash = BCrypt.hashpw(rawPassword, BCrypt.gensalt());
+        if (passwordHash.charAt(2) == 'y') {
+            //$2y - Replace the prefix to $2a in order to provide compatibility with bcrypt generated passwords
+            //from other implementations like in PHP
+            return passwordHash.substring(0, 2) + 'a' + passwordHash.substring(3, passwordHash.length());
+        }
+
+        return passwordHash;
     }
 
     @Override
     public boolean checkPassword(String passwordHash, String userInput) {
-        String checkedHash = passwordHash;
-        //$2y - Replace the prefix to $2a in order to provide compatibility with bcrypt generated passwords
-        //from other implementations like in PHP
-        if (passwordHash.charAt(2) == 'y') {
-            //http://stackoverflow.com/questions/27418597/bcrypt-version-1-1-2y-for-java
-            //skip one position in order to ignore the 'y'
-            checkedHash = passwordHash.substring(0, 2) + 'a' + passwordHash.substring(3, passwordHash.length());
-        }
-
-        return BCrypt.checkpw(userInput, checkedHash);
+        return BCrypt.checkpw(userInput, passwordHash);
     }
 }
